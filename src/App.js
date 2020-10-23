83f96d9660d4992bdaf28256469055d991c2c232
opauloh <paulo.scape@gmail.com>
2020-10-22 22:49:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ImageDemo from './ImageDemo';
+import ImageTransformDemo from './ImageTransformDemo';
+import VideoDemo from './VideoDemo';
+import ColourSelectDemo from './ColourSelectDemo';
+
+function App() {
+  return (
+    <>
+      <h1>React & Cloudinary</h1>
+      {/* <ImageDemo /> */}
+      {/* <ImageTransformDemo /> */}
+      {/* <VideoDemo /> */}
+      <ColourSelectDemo />
+    </>
+  );
+}
+
+export default App;
